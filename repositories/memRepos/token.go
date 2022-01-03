12b8c45c72953c04dbdd4d449fe24ce60040fd4e
Chechen Itza <artka.kazaktar@gmail.com>
2022-01-03 06:41:07 +0600

    add expiration functionality to memory token storage

--- repositories/memRepos/token.go
+++ repositories/memRepos/token.go
@@ -21,6 +21,7 @@ func NewToken(storage map[string]int32) *Token {
 func (t *Token) Get(uuid string) (int32, Cerr.CError) {
 	t.mu.Lock()
 	defer t.mu.Unlock()
+
 	userID, ok := t.storage[uuid]
 	if !ok {
 		return -1, Cerr.NewUnauthorized("token")
@@ -29,10 +30,18 @@ func (t *Token) Get(uuid string) (int32, Cerr.CError) {
 	return userID, nil
 }
 
+//Set method operates on the assumption that it won't be called on the same uuid more than once
+//Otherwise, a timer-based approach for deleting expired tokens needs to be implemented
 func (t *Token) Set(uuid string, userID int32, exp time.Duration) Cerr.CError {
 	t.mu.Lock()
 	defer t.mu.Unlock()
+
 	t.storage[uuid] = userID
+	//remove the token after expiration time has elapsed
+	go func() {
+		time.Sleep(exp)
+		_ = t.Del(uuid)
+	}()
 
 	return nil
 }
@@ -40,6 +49,7 @@ func (t *Token) Set(uuid string, userID int32, exp time.Duration) Cerr.CError {
 func (t *Token) Del(uuid string) Cerr.CError {
 	t.mu.Lock()
 	defer t.mu.Unlock()
+
 	delete(t.storage, uuid)
 
 	return nil
